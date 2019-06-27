2ddb380bb25a4eba204b35b6064c55ff9d54058b
John BEPPU <john.beppu@gmail.com>
2019-06-27 12:49:07 -0700

    Make it so that the .env file is optional

--- cmd/migrate/main.go
+++ cmd/migrate/main.go
@@ -12,9 +12,6 @@ import (
 
 func main() {
 	err := godotenv.Load()
-	if err != nil {
-		log.Fatal("Error loading .env file")
-	}
 	connect := fmt.Sprintf("host=%s user=%s dbname=%s password=%s", "localhost", os.Getenv("DB_USER"), os.Getenv("DB_NAME"), os.Getenv("DB_PASSWORD"))
 	db, err := gorm.Open("postgres", connect)
 	if err != nil {
