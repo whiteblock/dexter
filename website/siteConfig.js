cd0bdffd511b4fc1143e369733b063fa34c3bbc1
John BEPPU <john.beppu@gmail.com>
2019-08-02 13:21:00 -0700

    Change basic config of docusaurus site

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -21,17 +21,17 @@ const users = [
 ];
 
 const siteConfig = {
-  title: 'Test Site', // Title for your website.
-  tagline: 'A website for testing',
-  url: 'https://your-docusaurus-test-site.com', // Your website URL
-  baseUrl: '/', // Base URL for your project */
+  title: 'DEXter', // Title for your website.
+  tagline: 'Market Alerts as a Service',
+  url: 'https://whiteblock.github.io', // Your website URL
+  baseUrl: '/dexter/', // Base URL for your project */
   // For github.io type URLs, you would set the url and baseUrl like:
   //   url: 'https://facebook.github.io',
   //   baseUrl: '/test-site/',
 
   // Used for publishing and more
-  projectName: 'test-site',
-  organizationName: 'facebook',
+  projectName: 'dexter',
+  organizationName: 'whiteblock',
   // For top-level user or org sites, the organization is still the same.
   // e.g., for the https://JoelMarcey.github.io site, it would be set like...
   //   organizationName: 'JoelMarcey'
